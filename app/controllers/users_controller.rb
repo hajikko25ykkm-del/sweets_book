938d380df83bab4bace381cabd16dde508d6b697
YukaKuma <236227214+hajikko25ykkm-del@users.noreply.github.com>
2026-02-01 15:42:54 +0000

    user show,edit追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,34 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit]
+
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
+    @user = current_user
   end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "アカウント情報を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice: "退会が完了しました", status: :see_other
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :privacy)
+  end
+
 end
