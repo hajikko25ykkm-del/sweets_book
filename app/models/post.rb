de61e0d8934a06d2baeb214423063f244c3967f4
YukaKuma <236227214+hajikko25ykkm-del@users.noreply.github.com>
2026-03-06 15:47:55 +0000

    レイアウト調整、一覧表示の統一

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,4 +17,7 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_by, through: :favorites, source: :user
 
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
